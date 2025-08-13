a96a5ddce7c506c5cc6546002c788c4bd7e45a6f
pc <pc@localhost.localdomain>
2025-08-12 22:09:38 -0500

    Added FM demodulator page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,16 @@ import mdx from '@astrojs/mdx';
 
 import worker from '@astropub/worker'
 
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://emmanuel-herrera-oss.github.io',
-  integrations: [preact(), mdx(), worker()]
+  integrations: [preact(), mdx(), worker()],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
